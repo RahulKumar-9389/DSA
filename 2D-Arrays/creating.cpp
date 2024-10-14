4a9da882c8e431c0289cd1f9ac9f926d8b94d213
Rahul~Kumar <rahulkumar.programmer@gmail.com>
2024-10-14 20:35:39 +0530

    Creating 2D array

---
+++ 2D-Arrays/creating.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int arr[3][4] = {{1, 11, 111, 1111}, {2, 22, 222, 2222}, {3, 33, 333, 3333}};
+
+    // Print 2D array
+
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 4; j++)
+        {
+            cout << arr[i][j] << "  ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
