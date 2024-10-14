6538523496c41f61870ffc8406866bd3198431dd
Rahul~Kumar <rahulkumar.programmer@gmail.com>
2024-10-14 20:44:00 +0530

    Perform linear search in 2D array

--- 2D-Arrays/creating.cpp
+++ 2D-Arrays/creating.cpp
@@ -6,8 +6,7 @@ int main()
 
     int arr[3][4] = {{1, 11, 111, 1111}, {2, 22, 222, 2222}, {3, 33, 333, 3333}};
 
-    // Print 2D array
-
+    // Print 2D array || We can also take input as same to print array use cin>> instead of cout<<
     for (int i = 0; i < 3; i++)
     {
         for (int j = 0; j < 4; j++)
