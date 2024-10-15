1c59d96fd5ba06e82e3ab02ff07995a49468119f
Rahul~Kumar <rahulkumar.programmer@gmail.com>
2024-10-15 17:02:16 +0530

    Transpose a n*n 2D Array

---
+++ 2D-Arrays/printTranspose.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void printTranspose(int arr[][3], int rows, int cols)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = i + 1; j < cols; j++) // Only swap the upper triangle
+        {
+            // Swap arr[i][j] and arr[j][i]
+            swap(arr[i][j], arr[j][i]);
+        }
+    }
+}
+
+int main()
+{
+    int arr[3][3] = {
+        {1, 2, 3},
+        {4, 5, 6},
+        {7, 8, 9}};
+
+    printTranspose(arr, 3, 3);
+
+    // Print the transposed matrix
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
