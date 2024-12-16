56e21134988b69e8525ff2051c488ebea040e535
Rahul~Kumar <rahulkumar.programmer@gmail.com>
2024-12-16 21:01:32 +0530

    Converted sorted array to balanced binary tree

---
+++ Tree/arrayToTree.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+
+    // Constructor
+    Node(int data)
+    {
+        this->data = data;
+        this->left = this->right = nullptr;
+    }
+};
+
+Node *solve(int arr[], int start, int end)
+{
+    if (start > end)
+    {
+        return nullptr;
+    }
+
+    int mid = start + ((end - start) / 2);
+
+    // Create a new node
+    Node *root = new Node(arr[mid]);
+
+    // create left
+    root->left = solve(arr, start, mid - 1);
+
+    // create right
+    root->right = solve(arr, mid + 1, end);
+
+    return root;
+}
+
+Node *arrayToTree(int arr[])
+{
+    return solve(arr, 0, 4);
+}
+
+int main()
+{
+
+    int arr[5] = {-10, -3, 0, 5, 9};
+    return 0;
+}
