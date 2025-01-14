e284fab312828054e88b9a631237fbc2b7f7f2ea
Rahul~Kumar <rahulkumar.programmer@gmail.com>
2025-01-14 21:36:08 +0530

    Sort array using Insertion sort

---
+++ Sorting/insertionSort.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+int main()
+{
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    for (int i = 1; i < n; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+    for (int i = 0; i < n; i++)
+    {
+        std::cout << arr[i] << " ";
+    }
+    return 0;
+}
